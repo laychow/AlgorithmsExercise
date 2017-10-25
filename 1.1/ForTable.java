9da93a46b729b6c395e91920d4815b96401eb0a6
laychow <laychow94@163.com>
2017-10-25 20:50:32 +0800

    version2

--- 1.1/ForTable.java
+++ 1.1/ForTable.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
 
 public class ForTable {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner in = new Scanner(System.in);
+		System.out.print("请输入姓名和两个整数：（以空格分开）");
+		String name = in.next();
+		int a = in.nextInt();
+		int b = in.nextInt();
+		double c = (double)a/b;
+		System.out.printf("%5s%5d%5d%5.3f",name,a,b,c);
+		//System.out.println(c);
 	}
 
 }
